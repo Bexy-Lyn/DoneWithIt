307040f19074b2b30eae91ff9859894000ee3b2c
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-02-28 10:13:37 +0100

    extraction of default styles and implementation of an input component

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
+import Screen from "./app/components/Screen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import MyAccountScreen from "./app/screens/MyAccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Input from "./app/components/Input";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <Input placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
--- app/components/Paragraph.js
+++ app/components/Paragraph.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, Platform } from "react-native";
-import colors from "../config/colors";
+import { Text } from "react-native";
+import defaultStyles from "../config/styles";
 
 export default function Paragraph({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    color: colors.black,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
-});
