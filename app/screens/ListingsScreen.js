9790b8a3c3c14fc2514c24e02ee29afb25dcf371
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-02-28 09:24:54 +0100

    Listings improved with flatlist

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,30 +1,49 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: "100",
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: "1000",
+    image: require("../assets/couch.jpg"),
+  },
+];
+
 export default function ListingsScreen() {
   return (
     <Screen style={styles.screen}>
-      <Card
-        image={require("../assets/jacket.jpg")}
-        title="Red jacket for sale"
-        subTitle="$100"
-      />
-      <Card
-        image={require("../assets/couch.jpg")}
-        title="Couch in great condition"
-        subTitle="$1000"
+      <FlatList
+        style={styles.list}
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$" + item.price}
+            image={item.image}
+          />
+        )}
       />
     </Screen>
   );
 }
 const styles = StyleSheet.create({
   screen: {
-    marginTop: 20,
+    padding: 20,
     backgroundColor: colors.light,
-    paddingHorizontal: 20,
+  },
+  list: {
+    paddingTop: 20,
   },
 });
