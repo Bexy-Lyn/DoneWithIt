d4cbf896a7d7842f2116435f33c891ad4f4b03c6
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-03-02 08:41:13 +0100

    minor changes in Screen component

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Constants from "expo-constants";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.screen, style]}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   screen: {
     paddingTop: Constants.statusBarHeight,
     flex: 1,
