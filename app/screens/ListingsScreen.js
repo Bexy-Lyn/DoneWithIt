a3efa26b3cccc3185e34f7c07a435d29027284d2
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-03-08 15:27:16 +0100

    network error handling

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -6,19 +6,34 @@ import Screen from "../components/Screen";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import listingsApi from "../api/listings";
+import Paragraph from "../components/Paragraph";
+import Button from "../components/Button";
 
 export default function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   loadListings = async () => {
     const res = await listingsApi.getListings();
-    setListings(res.data);
+    if (!res.ok) return setError(true);
+    else {
+      setError(false);
+      setListings(res.data);
+    }
   };
 
   useEffect(() => loadListings(), []);
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <Paragraph style={{ textAlign: "center", padding: 10 }}>
+            Couldn't retrieve the listings.
+          </Paragraph>
+          <Button onPress={loadListings}>Retry</Button>
+        </>
+      )}
       <FlatList
         style={styles.list}
         data={listings}
